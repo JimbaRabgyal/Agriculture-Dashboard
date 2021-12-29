ef18124c79804d1d833e9549c7a1b5c0f0cb723c
Jimba Rabgyal <60665011+JimbaRabgyal@users.noreply.github.com>
2021-12-29 14:03:28 +0600

    updated .sh file

--- setup.sh
+++ setup.sh
@@ -4,15 +4,19 @@ echo "\
 email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
 
-echo "
-[theme]
-primaryColor = "#ffffff"
-backgroundColor = "#ccdede"
-secondaryBackgroundColor = "#99cccc"
-textColor = "#010100"
-font = "sans serif"
-[server]
-headless = true
-enableCORS=false
-port = $PORT
+echo "\
+[server]\n\
+port = $PORT\n\
+enableCORS = false\n\
+headless = true\n\
+\n\
 " > ~/.streamlit/config.toml
+
+echo "[theme]
+primaryColor="#35793b"
+backgroundColor="#ccdede"
+secondaryBackgroundColor="#99cccc"
+textColor="#010100"
+" > ~/.streamlit/config.toml
+
+
